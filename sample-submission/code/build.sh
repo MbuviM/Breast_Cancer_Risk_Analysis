a4d2389049453e63ca10ce93592e0e2837d86f20
DataNerd <mwendembuvi10@gmail.com>
2022-10-25 17:35:19 +0300

    Initial Commit

---
+++ sample-submission/code/build.sh
@@ -0,0 +1,7 @@
+apt-get update && \
+        apt-get install -y --no-install-recommends \
+        python3.9 \
+        python3-pip \
+        python3-pandas \
+        python3-sklearn \
+        software-properties-common
---
+++ sample-submission/code/train.sh
@@ -0,0 +1,2 @@
+python3 src/sample/submission/trainer.py "$1" model/dummy-model.pickle
+
