4fd70a489d46859d2fa56a7e6106274733a9fef0
Yvonne Mbuvi <mwendembuvi10@gmail.com>
2022-10-29 20:38:10 +0300

    Delete build.sh
    Not needed.

--- sample-submission/code/build.sh
+++
@@ -1,7 +0,0 @@
-apt-get update && \
-        apt-get install -y --no-install-recommends \
-        python3.9 \
-        python3-pip \
-        python3-pandas \
-        python3-sklearn \
-        software-properties-common
